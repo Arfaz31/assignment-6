d2f17e5f1a54364bfd22500972833bdbd5ca31ae
ArfazAhamed <arfazahamed31@gmail.com>
2023-03-04 15:04:24 +0600

    see-more

--- js/ai-hub.js
+++ js/ai-hub.js
@@ -2,12 +2,13 @@ const loadAiHub = async() =>{
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    displayAi(data.data.tools);
+    displayAi(data.data.tools.slice(0,6));
 
 }
 
 const displayAi = aiHubs =>{
     const aiContainer = document.getElementById('ai-container');
+   
     aiHubs.forEach(ai => {
         const aiDiv = document.createElement('div')
         aiDiv.classList.add('col');
@@ -16,19 +17,26 @@ const displayAi = aiHubs =>{
       <img src="${ai.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Features</h5>
+      
         <div>
         <ol class="list-group-numbered p-0">
   <li class="list-group-item">${ai.features[0]}</li>
   <li class="list-group-item">${ai.features[1]}</li>
   <li class="list-group-item">${ai.features[2]}</li>
- 
 </ol>
-        </div>
-      </div>
-      <div class="card-footer">
+</div>
+
+<hr class="border text-secondary opacity-50"> 
+
+      <div class="d-flex justify-content-between">
+      <div class=" ">
         <p class="fw-bolder">${ai.name}</p>
-        <p class="fs-6">${ai.published_in}</p>
+        <p class="fs-6 "><i class="fa-solid fa-calendar-days"></i> ${ai.published_in}</p>
       </div>
+      <div>
+      <button type="button" class="btn btn-outline-danger">More Info</button>
+      </div>   
+    </div>
     </div>
   </div>
         
@@ -38,5 +46,15 @@ const displayAi = aiHubs =>{
     });
 }
 
-
 loadAiHub();
+
+// show all data
+document.getElementById('btn-see-more').addEventListener('click', function(){
+  fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(response=> response.json())
+    .then(data=> displayAi(data.data.tools))
+    const hideButton= document.getElementById("see-more");
+hideButton.classList.add('d-none')
+
+})
+
